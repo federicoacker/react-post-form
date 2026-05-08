d30f00e938e9ea27ade71eb153657be651430f0c
federicoacker <acker.federico@gmail.com>
2026-05-08 15:28:59 +0200

    Fatto il jsx di base della form

--- src/components/Main/AddPostForm.jsx
+++ src/components/Main/AddPostForm.jsx
@@ -1,13 +1,24 @@
 import { Col } from "react-bootstrap";
 
-function AddPostForm({posts}) {
-  return (
-    <Col xs={12} sm={12} md={6}>
-        <form>
-            
-        </form>
-    </Col>
-  )
+function AddPostForm({ posts }) {
+    return (
+        <Col xs={12} sm={12} md={6}>
+            <form className="form-control d-flex flex-column justify-content-center">
+                <label className="form-label" htmlFor="title">Titolo del Post</label>
+                <input className="form-control" type="text" name="title" id="title" />
+                <label className="form-label" htmlFor="author">Autore</label>
+                <input className="form-control" type="text" name="author" id="author" />
+                <label className="form-label" htmlFor="body">Testo del Post</label>
+                <textarea className="form-control" name="body" id="body" />
+                <div>
+                    <label className="form-check-label" htmlFor="public">Voglio che il mio post sia visualizzabile da altri utenti</label>
+                    <input className="form-check-input mx-3" type="checkbox" name="public" id="public" />
+                    <p className="form-check-label" >(Potrebbero visualizzarlo lo stesso se non hanno implementato i controlli giusti)</p>
+                </div>
+                <button className="btn btn-primary">Invia il tuo post nell'interweb!</button>
+            </form>
+        </Col>
+    )
 }
 
 export default AddPostForm
