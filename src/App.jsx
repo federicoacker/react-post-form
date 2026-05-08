78a0e48a0ba18e44e7fd22c843ed0d0e79a4b610
federicoacker <acker.federico@gmail.com>
2026-05-08 17:20:28 +0200

    Switchato loaded come stato di base a true perché ho pensato. Se nella useEffect, tra le dependency ho loaded e lo setto a true dentro la callback. Ma parto con loaded = false. Allora al mount la useEffect verrà chiamata 2 volte, la prima perché è il mount, la seconda perché loaded è stato cambiato a true dentro la useEffect. Così invece se metto loaded a true dall'inizio, la useEffect che mi fa la fetch viene chiamata lo stesso al mount, però poi i successivi richiami avvengono effettivamente solo quando loaded viene cambiato (ovvero al submit del post)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {useState} from "react";
 
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
+  const [loaded, setLoaded] = useState(true);
   return (
     <>
     <Header/>
