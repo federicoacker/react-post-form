73af5e228b0d5931e97e941ca83c11b49de3598e
federicoacker <acker.federico@gmail.com>
2026-05-08 15:05:59 +0200

    Fatto un minimo di stile per rendere più interessanti i singoli post, ora passo alla form

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,19 +2,24 @@ import { getPosts } from "../../utils/fetch.js";
 import { useState, useEffect } from "react";
 
 import Postlist from "./Postlist.jsx";
+import { Container, Row } from "react-bootstrap";
 
 
 function Main() {
     const [posts, setPosts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         getPosts().then(posts => setPosts(posts));
-    },[]);
+    }, []);
 
     return (
-        <div>
-            <Postlist posts={ posts }/>
-        </div>
+        <main>
+            <Container>
+                <Row className="g-4 row-gap-2">
+                    <Postlist posts={posts} />
+                </Row>
+            </Container>
+        </main>
     );
 }
 
--- src/components/Main/Post.jsx
+++ src/components/Main/Post.jsx
@@ -1,16 +1,19 @@
+import { Col } from "react-bootstrap"
 
-function Post({author, title, body}) {
-  return (
-    <div className="single-post">
-        <div className="post-header">
-            <h2> {title} </h2>
-        </div>
-        <div className="post-body">
-            <p>By - {author}</p>
-            <p>{body}</p>
-        </div>
-    </div>
-  )
+function Post({ author, title, body }) {
+    return (
+        <Col xs={12} sm={12} md={6} lg={4}>
+            <div className="single-post border rounded border-danger bg-dark text-white h-100">
+                <div className="post-header border-bottom border-danger p-3">
+                    <h2> {title} </h2>
+                </div>
+                <div className="post-body p-3">
+                    <p>By - {author}</p>
+                    <p>{body}</p>
+                </div>
+            </div>
+        </Col>
+    )
 }
 
 export default Post
